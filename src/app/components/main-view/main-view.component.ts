b6b7399ddb023b37d3376487ddfc0a2d2b4921aa
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-23 10:33:20 +0200

    ..

--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, Inject, ViewChild} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
-import {ToastrService} from "toastr-ng2";
+import { ToastrService } from 'ngx-toastr';
 import {Observable} from "rxjs/Rx";
 import {GetComponent} from "./get/get.component";
 @Component({
