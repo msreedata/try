662fd2d602cfa0bb919c05507564b652b2c8be5e
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-23 09:09:31 +0200

    ..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { RouterModule } from '@angular/router';
 
 const routes = [
-  {path: '', loadChildren: 'components/main-view/main-view.module#MainViewModule'}
+  {path: '', loadChildren: './components/main-view/main-view.module#MainViewModule'}
 ];
 
 export const Routing = RouterModule.forRoot(routes, { useHash: true });
